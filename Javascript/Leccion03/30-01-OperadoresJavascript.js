c27835f3276b66bcc259daf851d622c97773b603
JuanPerez <juan.perez@pi.edu.co>
2022-03-09 14:28:30 -0500

    Operadores en javascript

---
+++ Javascript/Leccion03/30-01-OperadoresJavascript.js
@@ -0,0 +1,21 @@
+//Operadores aritméticos en javascript.
+
+let a = 3,
+    b = 2;
+let z = a + b;
+console.log("Resultado de la suma es: " + z);
+
+z = a - b;
+console.log("Resultado de la resta es: " + z);
+
+z = a * b;
+console.log("Resultado de la multiplicación es: " + z);
+
+z = a / b;
+console.log("Resultado de la división es: " + z);
+
+z = a % b; // residuo de la división.
+console.log("el residuo de la división es: " + z);
+
+z = a ** b;
+console.log("Resultado del exponente es: " + z);
