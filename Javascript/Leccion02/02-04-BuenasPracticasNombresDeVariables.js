7f9f9d4c0b0bf4e1d141133339af448ab5f6375c
JuanPerez <juan.perez@pi.edu.co>
2022-03-09 13:58:17 -0500

    Buenas practicas declarando nombres de variables en javascript

---
+++ Javascript/Leccion02/02-04-BuenasPracticasNombresDeVariables.js
@@ -0,0 +1,22 @@
+//notación de altas y bajas la primera palabra comiennza con minuscula y las siguientes palabras comienzan en mayusculas.
+let nombreCompleto = "Juan Perez";
+console.log(nombreCompleto);
+
+let x, y;
+x = 10, y = 20;
+let z = x + y;
+console.log(z);
+
+// En javascriptlas mayusculas y minusculas si Importan.
+let apellidoCompleto = "Perez Santana";
+let apellidocompleto = "Arrieta Perez";
+console.log(apellidoCompleto);
+console.log(apellidocompleto);
+
+// Reglas para declarar el nombre de una variable el nombre no puede comenzar con un numero solo se puede utilizar mayuscalas, minusculas,_,$ y luego de esto si podemos usar nuneros.
+let a1nombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto
+    //let 1nombreCompleto ¡INCORRECTO!
+
+// Una variable no se puede definir con los nombres de las palabras reservadas de javascript.
