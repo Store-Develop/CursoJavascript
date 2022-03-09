bd87711578be961e34d44f369a47c56e0abaf0c0
JuanPerez <juan.perez@pi.edu.co>
2022-03-09 15:01:29 -0500

    Operadores de incremento y decremento en javascript

---
+++ Javascript/Leccion03/30-02-OperadoresDeIncrementoYDecrementoJavascript.js
@@ -0,0 +1,27 @@
+// Operadores de Incremeto Y Decremento Javascript.
+
+let a = 3,
+    b = 2;
+let z = a + b;
+
+//Incremento
+//pre-incremento(el operador ++ antes de la variable)
+z = ++a;
+console.log(a);
+console.log(z);
+
+//post-incremento(el operador ++ despues de la variable)
+z = b++;
+console.log(b);
+console.log(z);
+
+
+//pre-decremento(el operador -- antes de la variable)
+z = --a;
+console.log(a);
+console.log(z);
+
+//post-decremento(el operador -- despues de la variable)
+z = b--;
+console.log(b);
+console.log(z);
