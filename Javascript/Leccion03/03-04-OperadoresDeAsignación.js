d0a6e507840f146d902b7c5fe4560a0042f7b9e0
JuanPerez <juan.perez@pi.edu.co>
2022-03-11 14:08:24 -0500

    Operadores de asignación en javascript

---
+++ Javascript/Leccion03/03-04-OperadoresDeAsignación.js
@@ -0,0 +1,15 @@
+// Operador de asignación (=).
+let a = 1;
+
+//Operador de asignación compuesto (=+), (-=).
+a += 3; //Es igual a decir que (a = a + 3).
+console.log(a);
+
+a -= 2; // Es igual a decir que (a = a - 2).
+console.log(a);
+/*
+*= Multiplicación.
+/= División.
+%= Modulo.
+**= Exponente.
+*/
