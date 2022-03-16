6e0e684dfc8dc56776c07752ae071a386ff03e7e
JuanPerez <juan.perez@pi.edu.co>
2022-03-16 12:40:04 -0500

    Ejercicio número par o impar en javasctript

---
+++ Javascript/Leccion03/03-08-EjercicioNumeroParEnJavascript.js
@@ -0,0 +1,13 @@
+let a = 10;
+
+if (a % 2 == 0) {
+    console.log("Es un número par.");
+} else {
+    console.log("Es un número impar.");
+}
+a = 9;
+if (a % 2 == 0) {
+    console.log("Es un número par.");
+} else {
+    console.log("Es un número impar.");
+}
