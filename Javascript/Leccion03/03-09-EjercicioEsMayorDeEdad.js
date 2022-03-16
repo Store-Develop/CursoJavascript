7c2f044aac723d495bcb168d6760983e86c13026
JuanPerez <juan.perez@pi.edu.co>
2022-03-16 13:03:26 -0500

    Ejercicio es mayor o menor de edad en javascript

---
+++ Javascript/Leccion03/03-09-EjercicioEsMayorDeEdad.js
@@ -0,0 +1,24 @@
+let edad = 20,
+    adulto = 18;
+
+if (edad >= adulto) {
+    console.log("Es un adulto");
+} else {
+    console.log("Es menor de edad");
+}
+
+edad = 17;
+
+if (edad >= adulto) {
+    console.log("Es un adulto");
+} else {
+    console.log("Es menor de edad");
+}
+
+edad = 18;
+
+if (edad >= adulto) {
+    console.log("Es un adulto");
+} else {
+    console.log("Es menor de edad");
+}
