8da5e6653369c319cdc362ef1b6129d47a99a4f4
JuanPerez <juan.perez@pi.edu.co>
2022-03-16 13:27:35 -0500

    Operador lógico AND en javascript

---
+++ Javascript/Leccion03/03-10-OperadorLogicoAND.js
@@ -0,0 +1,50 @@
+//En javascript la representación de (AND) es (&&) y para que se cumpla ambos resultados deben ser verdaderos
+let a = 5;
+let valMin = 0,
+    valMax = 10;
+
+if (a >= valMin && a <= valMax) {
+    console.log("Dentro de rango.");
+} else {
+    console.log("Fuera de rango.");
+}
+
+a = -1,
+    valMin = 0,
+    valMax = 10;
+
+if (a >= valMin && a <= valMax) {
+    console.log("Dentro de rango.");
+} else {
+    console.log("Fuera de rango.");
+}
+
+a = 15,
+    valMin = 0,
+    valMax = 10;
+
+if (a >= valMin && a <= valMax) {
+    console.log("Dentro de rango.");
+} else {
+    console.log("Fuera de rango.");
+}
+
+a = 10,
+    valMin = 0,
+    valMax = 10;
+
+if (a >= valMin && a <= valMax) {
+    console.log("Dentro de rango.");
+} else {
+    console.log("Fuera de rango.");
+}
+
+a = 0,
+    valMin = 0,
+    valMax = 10;
+
+if (a >= valMin && a <= valMax) {
+    console.log("Dentro de rango.");
+} else {
+    console.log("Fuera de rango.");
+}
