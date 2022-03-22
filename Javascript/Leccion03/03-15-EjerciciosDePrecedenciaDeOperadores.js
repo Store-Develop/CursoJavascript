fb7c37e90f408625f00a22c2821fbae841ff1d84
JuanPerez <juan.perez@pi.edu.co>
2022-03-22 16:00:33 -0500

    Ejercicios de precedencia de operadores en jaavscript

---
+++ Javascript/Leccion03/03-15-EjerciciosDePrecedenciaDeOperadores.js
@@ -0,0 +1,14 @@
+// Incremento y decremento.
+let x = 5;
+let y = 10;
+let z = ++x + y--;
+console.log(x);
+console.log(y);
+console.log(z);
+
+//prioridad de signos en una operación.
+let resultado = 4 + 5 * 6 / 3;
+console.log(resultado);
+
+resultado = (4 + 5) * 6 / 3;
+console.log(resultado);
