3d06e9bee46c3d2e86614916af605f9ae409ba52
JuanPerez <juan.perez@pi.edu.co>
2022-03-22 14:59:01 -0500

    Operador ternario en javascript

---
+++ Javascript/Leccion03/03-12-OperadorTernario.js
@@ -0,0 +1,9 @@
+//El operador ternario en javascript es representado por el signo (?) y tiene casi la misma funcionalidad que el el (if else).
+
+let resultado = (1 > 2) ? "verdadero" : "falso";
+console.log(resultado);
+
+//numero es par o impar con operador ternario.
+let numero = 9;
+resultado = (numero % 2 == 0) ? "Numero par" : "Numero Impar";
+console.log(resultado);
