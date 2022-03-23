a9736d1fbfa762fa01b2c7253ffc7a4fda0ddfbb
JuanPerez <juan.perez@pi.edu.co>
2022-03-23 14:15:18 -0500

    Ejemplo If/Else en javascript

---
+++ Javascript/Leccion04/04-02-Ejemplo(IfElse).js
@@ -0,0 +1,24 @@
+let numero = 11;
+if (numero == 1) {
+    console.log("Número uno");
+} else if (numero == 2) {
+    console.log("Número dos");
+} else if (numero == 3) {
+    console.log("Número tres");
+} else if (numero == 4) {
+    console.log("Número cuatro");
+} else if (numero == 5) {
+    console.log("Número cinco");
+} else if (numero == 6) {
+    console.log("Número seis");
+} else if (numero == 7) {
+    console.log("Número siete");
+} else if (numero == 8) {
+    console.log("Número ocho");
+} else if (numero == 9) {
+    console.log("Número nueve");
+} else if (numero == 10) {
+    console.log("Número dies");
+} else {
+    console.log("Número desconocido");
+}
