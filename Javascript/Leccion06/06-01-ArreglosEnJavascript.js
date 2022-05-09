1229b5c5918f912c1e1a814955aa24190d2fcb40
JuanPerez <juan.perez@pi.edu.co>
2022-05-09 15:03:03 -0500

    Areglos en javascript

---
+++ Javascript/Leccion06/06-01-ArreglosEnJavascript.js
@@ -0,0 +1,5 @@
+//Existen dos formas para declarar un arreglo en javascript.
+
+//let autos = new Array('BMW', 'Mercedes Benz', 'Volvo'); Está es la forma clasica de declararlos.
+const autos = ['BMW', 'Mercedes Benz', 'Volvo']; // forma actual.
+console.log(autos);
