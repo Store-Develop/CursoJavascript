4b423be72d8f7bcaa69489c6e5982ae955ad4406
JuanPerez <juan.perez@pi.edu.co>
2022-05-12 17:01:56 -0500

    Funciones en javascript

---
+++ Javascript/Leccion07/07-01-FuncionesEnJavascript.js
@@ -0,0 +1,9 @@
+miFuncion(4, 2); // Llamando la función.
+
+//Declaración de la función.
+function miFuncion(a, b) {
+    console.log("Suma: " + (a + b));
+}
+
+// Llamando la función.
+miFuncion(2, 3);
