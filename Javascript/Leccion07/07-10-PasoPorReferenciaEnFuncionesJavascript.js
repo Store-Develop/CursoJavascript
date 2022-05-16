b823ab8e1417946f5a29fe1b41838cf7ef470a26
JuanPerez <juan.perez@pi.edu.co>
2022-05-16 16:28:53 -0500

    Paso por refencia en funciones javascript

---
+++ Javascript/Leccion07/07-10-PasoPorReferenciaEnFuncionesJavascript.js
@@ -0,0 +1,26 @@
+//Tipos primitivos.
+let x = 10;
+
+function cambiarValor(a) {
+    a = 20;
+}
+
+//Paso por valor.
+cambiarValor(x); //10
+console.log(x);
+//console.log(a);
+
+//Creamos el metodo para realizar un paso por referencia.
+const persona = {
+    nombre: 'Juan',
+    apellido: 'Perez'
+}
+
+function cambiarValorObjeto(p1) {
+    p1.nombre = 'Carlos';
+    p1.apellido = 'Lara';
+}
+
+//Paso por referencia
+cambiarValorObjeto(persona);
+console.log(persona);
