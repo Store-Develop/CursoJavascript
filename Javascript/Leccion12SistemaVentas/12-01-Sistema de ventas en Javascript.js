b62ded38b5cb0d88b950cb618f85c4c9a8e6b720
JuanPerez <juan.perez@pi.edu.co>
2022-05-23 22:08:35 -0500

    Codigo de proyecto terminado a la espera de pruebas de funcionalidades

--- Javascript/Leccion12SistemaVentas/12-01-Sistema de ventas en Javascript.js
+++ Javascript/Leccion12SistemaVentas/12-01-Sistema de ventas en Javascript.js
@@ -32,6 +32,50 @@ class Producto {
     }
 }
 
+class Orden {
+    static contadorOrdenes = 0;
+
+    static get MAX_PRODUCTOS() {
+        return 5;
+    }
+
+    constructor() {
+        this._idOrden = ++Orden.contadorOrdenes;
+        this._productos = [];
+        this._contadorProductosAgregados = 0;
+    }
+
+    get idOrden() {
+        return this._idOrden;
+    }
+
+    agregarProducto(producto) {
+        if (this._productos.length < Orden.MAX_PRODUCTOS) {
+            this._productos.push(producto);
+            //this._productos[this.contadorProductosAgregados++] = producto;
+        } else {
+            console.log('No se pueden agregar más productos');
+        }
+    }
+
+    calcularTotal() {
+        let totalVentas = 0;
+        for (let producto of this._productos) {
+            totalVentas += producto.precio; // totalVentas = totalVentas + producto.precio;
+        }
+        return totalVentas;
+    }
+
+    mostrarOrden() {
+        let productosOrden = '';
+        for (let producto of this._Producto) {
+            productosOrden += producto.toString() + ' ';
+        }
+
+        console.log(`Orden: ${this._idOrden} Total: ${calcularTotal()}, Productos: ${productosOrden}`);
+    }
+}
+
 let producto1 = new Producto('Pantalon', 200);
 let producto2 = new Producto('Camisa', 100);
 console.log(producto1.toString());
