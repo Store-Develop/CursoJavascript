4004c622d9515a029fe296a35fbdafad809e5046
JuanPerez <juan.perez@pi.edu.co>
2022-05-26 17:12:58 -0500

    Uso del bloque try catch finally en javascript

---
+++ Javascript/Leccion16/16-01-BloqueTryCatchFinallyEnJavascript.js
@@ -0,0 +1,12 @@
+"use strict";
+try {
+    let x = 10;
+    //miFincion();
+
+} catch (error) {
+    console.log(error);
+} finally {
+    console.log('Termina la revisión de errores');
+}
+
+console.log('Continuamos...');
