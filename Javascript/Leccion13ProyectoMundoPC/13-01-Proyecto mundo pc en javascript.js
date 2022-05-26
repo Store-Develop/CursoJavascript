c0b668fb0369613e6ce131756af904ae1edc051d
JuanPerez <juan.perez@pi.edu.co>
2022-05-25 21:11:34 -0500

    Proyecto mundo pc terminado y probado

--- Javascript/Leccion13ProyectoMundoPC/13-01-Proyecto mundo pc en javascript.js
+++ Javascript/Leccion13ProyectoMundoPC/13-01-Proyecto mundo pc en javascript.js
@@ -28,10 +28,12 @@ class Raton extends DispositivoEntrada {
         this._idRato = ++Raton.contadorRatones;
     }
 
+    get idRaton() {
+        return this._idRato;
+    }
+
     toString() {
-        return `Tipo de entrada: ${this._tipoEntrada}.
-        Marca: ${this._marca}.
-        idRaton: ${this._idRato}.`;
+        return `Raton: [idRaton: ${this._idRato} Tipo entrada: ${this._tipoEntrada} Marca: ${this._marca}]`;
     }
 }
 
@@ -42,10 +44,12 @@ class Teclado extends DispositivoEntrada {
         this._idTeclado = ++Teclado.contardorTeclados;
     }
 
+    get idTeclado() {
+        return this._idTeclado;
+    }
+
     toString() {
-        return `Tipo de entrada: ${this._tipoEntrada}.
-        Marca: ${this._marca}.
-        idTeclado: ${this._idTeclado}.`;
+        return `Teclado: [idTeclado: ${this._idTeclado  } Tipo entrada: ${this._tipoEntrada} Marca: ${this._marca}]`;
     }
 }
 
@@ -74,10 +78,82 @@ class Monitor {
     }
 
     toString() {
-        return `Marca monitor: ${this._marca}.
-        Tamaño: ${this._tamaño}.
-        idMonitor: ${this._idMonitor}.`;
+        return `Monitor: [idMonitor: ${this._idMonitor} Marca: ${this._marca} Tamaño: ${this._tamaño}]`;
     }
+}
+
+class Computadora {
+    static contadorComputadoras = 0;
+    constructor(nombre, raton, teclado, monitor) {
+        this._idComputadora = ++Computadora.contadorComputadoras;
+        this._nombre = nombre;
+        this._monitor = monitor;
+        this._teclado = teclado;
+        this._raton = raton;
+    }
+
+    get idComputadora() {
+        return this._idComputadora;
+
+    }
+    get nombre() {
+        return this._nombre;
+    }
+
+    set nombre(nombre) {
+        this._nombre = nombre;
+    }
+
+    get monitor() {
+        return this._monitor;
+    }
+    set monitor(monitor) {
+        this._monitor = monitor;
+    }
+    get teclado() {
+        return this._teclado;
+    }
+
+    set teclado(teclado) {
+        this._teclado = teclado;
+    }
+
+    get raton() {
+        return this._raton;
+    }
+
+    set raton(raton) {
+        this._raton = raton;
+    }
+
+    toString() {
+        return `Computadora: ${this._idComputadora}: ${this._nombre} \n ${this._raton} \n ${this._teclado} \n ${this._monitor}`;
+    }
+}
+
+class Orden {
+    static contadorOrdenes = 0;
+    constructor() {
+        this._idOrden = ++Orden.contadorOrdenes;
+        this._computadoras = [];
+    }
+
+    get idOrden() {
+        return this._idOrden;
+    }
+
+    agregarComputadora(computadora) {
+        this._computadoras.push(computadora);
+    }
+
+    mostrarOrden() {
+        let ordenComputadoras = '';
+        for (let computadoras of this._computadoras) {
+            ordenComputadoras += '\n{' + computadoras.toString() + '}';
+        }
+        console.log(`Orden: ${this._idOrden }, Computadoras: ${ordenComputadoras}`);
+    }
+
 }
 
 let entrada1 = new DispositivoEntrada('USB', 'HP');
@@ -96,3 +172,19 @@ let monitor1 = new Monitor('Lenovo', '27');
 console.log(monitor1.toString());
 let monitor2 = new Monitor('Dell', '18');
 console.log(monitor2.toString());
+let computadora1 = new Computadora('HP', raton1, teclado1, monitor1);
+console.log(computadora1.toString());
+let computadora2 = new Computadora('MAC', raton2, teclado2, monitor2);
+console.log(computadora2.toString());
+let orden1 = new Orden();
+orden1.agregarComputadora(computadora1);
+orden1.agregarComputadora(computadora2);
+
+orden1.mostrarOrden();
+
+let orden2 = new Orden();
+orden2.agregarComputadora(raton1);
+orden2.agregarComputadora(teclado1);
+orden2.agregarComputadora(monitor1);
+
+orden2.mostrarOrden();
