a997ce5d44fbf25f1a56980e139ae17700e5b31e
JuanPerez <juan.perez@pi.edu.co>
2022-05-29 12:41:26 -0500

    Palabra await y async con promesas en javascript

---
+++ Javascript/Leccion19/19-04-PalabraAwaitYAsyncConPromesasEnJavascript.js
@@ -0,0 +1,37 @@
+let miPromesa = new Promise((resolver, rechazar) => {
+    let expresion = true;
+    if (expresion) {
+        resolver('Resolvió correcto');
+    } else
+        rechazar('Se produjo un error');
+
+});
+
+//miPromesa.then(valor => console.log(valor), error => console.log(error));
+//miPromesa.then(valor => console.log(valor)).catch(error => console.log(error));
+
+let promesa = new Promise((resolver) => {
+    //console.log('Incio promesa');
+    setTimeout(() => resolver('Saludos con promesa y timeout'), 3000);
+    //console.log('Fin promesa');
+});
+
+//promesa.then(valor => console.log(valor));
+
+//Async indica que una función regresa una promesa
+
+async function miFuncionConPromesa() {
+    return 'Saludos con promesa y async';
+}
+//miFuncionConPromesa().then(valor => console.log(valor));
+
+//async/await
+async function funcionConPromesaYAwait() {
+    let miPromesa = new Promise(resolver => {
+        resolver('Promesa con await');
+    });
+
+    console.log(await miPromesa);
+}
+
+funcionConPromesaYAwait();
