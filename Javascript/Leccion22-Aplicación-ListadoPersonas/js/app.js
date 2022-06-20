b4aa61cb39cc6dbbf33042ad220dacc26f122d06
JuanPerez <jkml563@gmail.com>
2022-06-20 15:24:30 -0500

    Listado de Personas con Javascript

--- Javascript/Leccion22-Aplicación-ListadoPersonas/js/app.js
+++ Javascript/Leccion22-Aplicación-ListadoPersonas/js/app.js
@@ -1,1 +1,13 @@
-console.log('Listado de personas');
+const personas = [
+    new Persona('Juan', 'Pérez'), new Persona('Karla', 'Lara')
+];
+
+function mostrarPersonas() {
+    console.log('Mostrar Personas...');
+    let texto = '';
+    for (let persona of personas) {
+        console.log(persona);
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+        document.getElementById('personas').innerHTML = texto;
+    }
+}
--- Javascript/Leccion22-Aplicación-ListadoPersonas/js/persona.js
+++ Javascript/Leccion22-Aplicación-ListadoPersonas/js/persona.js
@@ -1,1 +1,19 @@
-console.log('Clase de Persona');
+class Persona {
+    constructor(nombre, apellido) {
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+    get nombre() {
+        return this._nombre;
+    }
+    set nombre(nombre) {
+        this._nombre = nombre;
+    }
+
+    get apellido() {
+        return this._apellido;
+    }
+    set apellido(apellido) {
+        this._apellido = apellido;
+    }
+}
