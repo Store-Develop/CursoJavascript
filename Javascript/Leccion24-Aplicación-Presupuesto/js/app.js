f790e3851d083925d299bf3ec3efe18186a424eb
JuanPerez <jkml563@gmail.com>
2022-06-21 18:34:55 -0500

    Cabecero con Javascript de la Aplicación Presupuesto

--- Javascript/Leccion24-Aplicación-Presupuesto/js/app.js
+++ Javascript/Leccion24-Aplicación-Presupuesto/js/app.js
@@ -1,5 +1,5 @@
 const ingresos = [
-    new Ingreso('Salario', 2100.00),
+    new Ingreso('Salario', 2000.00),
     new Ingreso('Venta coche', 1500)
 ];
 
@@ -12,4 +12,27 @@ let cargarApp = () => {
     cargarCabecero();
 }
 
-let cargarCabecero = () => {}
+let totalIngresos = () => {
+    let totalIngreso = 0;
+    for (let ingreso of ingresos) {
+        totalIngreso += ingreso.valor;
+    }
+    return totalIngreso;
+}
+
+let totalEgresos = () => {
+    let totalEgreso = 0;
+    for (let egreso of egresos) {
+        totalEgreso += egreso.valor;
+    }
+    return totalEgreso;
+}
+
+let cargarCabecero = () => {
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgreso = totalEgresos() / totalIngresos();
+    document.getElementById('presupuesto').innerHTML = presupuesto;
+    document.getElementById('porcentaje').innerHTML = porcentajeEgreso;
+    document.getElementById('ingresos').innerHTML = totalIngresos();
+    document.getElementById('egresos').innerHTML = totalEgresos();
+}
